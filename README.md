86864fc2b267f561ce9e5cf4df8a9a136083f8b2
Andrew Bates <andrewbates09@gmail.com>
2015-06-10 18:56:00 -0700

    upd readme, branching to test bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# wsucon
+# WSUCon
 WSUCon Website
+
+So far this is what the dev dependencies/setup looks like:
+1. git
+2. nodejs
+3. npm
+4. bower
+5. bootstrap
+6. sass
+7. font-awesome
+
+To contribute/work on it:
+* basically working on modification/combination of [handlebars](https://www.npmjs.com/package/gulp-handlebars) and [this](http://ericlbarnes.com/setting-gulp-bower-bootstrap-sass-fontawesome/) kiler setup.
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass')
+	notify = require('gulp-notify')
 	bower = require('gulp-bower');
 
 var config = {
-	sassPath: './source/_sass',
+	sassPath: './source/sass',
 	bowerDir: './bower_components'
 }
 
@@ -19,20 +20,20 @@ gulp.task('icons', function() {
 });
 
 
-gulp.task('css', function() { 
-    return gulp.src(config.sassPath + '/style.scss')
-         .pipe(sass({
-             style: 'compressed',
-             loadPath: [
-                 './source/sass',
-                 config.bowerDir + '/bootstrap-sass/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss',
-             ]
-         }) 
-            .on("error", notify.onError(function (error) {
-                 return "Error: " + error.message;
-             }))) 
-         .pipe(gulp.dest('./public/css')); 
+gulp.task('css', function() {
+	return gulp.src(config.sassPath + '/style.scss')
+		.pipe(sass({
+			style: 'compressed',
+			loadPath: [
+				'./source/sass',
+				config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+				config.bowerDir + '/fontawesome/scss',
+			]
+		})
+			.on("error", notify.onError(function (error) {
+				return "Error: " + error.message;
+			})))
+		.pipe(gulp.dest('./public/css'));
 });
 
 
